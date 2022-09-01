9638289e0c94efffeaa1e07353ddb630536ca7d9
Arka2001 <sayantan.dasgupta21@gmail.com>
2022-09-02 00:38:59 +0530

    Added Registration End Point

---
+++ authentication/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from rest_framework.generics import GenericAPIView
+from .serializers import UserSerializer
+from rest_framework.response import Response
+from rest_framework import status
+
+class RegisterView(GenericAPIView):
+    serializer_class = UserSerializer
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
